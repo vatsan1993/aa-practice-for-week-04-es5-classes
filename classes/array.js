0cf8a747ed087b84f12ee016f36a8a6d81ae6bb5
Srivatsan <vatsan1993@gmail.com>
2023-09-06 13:45:51 +0530

    finished the project

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,13 @@
-// Your code here
+Array.prototype.isEqual = function (arr) {
+  if (this.length !== arr.length) {
+    return false;
+  }
+  let isSame = true;
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] != arr[i]) {
+      isSame = false;
+      break;
+    }
+  }
+  return isSame;
+};
