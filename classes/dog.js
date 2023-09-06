3079a3e329f21b8881cb5563fa83e3a5aa0ea762
Srivatsan <vatsan1993@gmail.com>
2023-09-06 13:14:44 +0530

    finished coding dog.js

--- classes/dog.js
+++ classes/dog.js
@@ -1,17 +1,29 @@
-class Dog {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// class Dog {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  bark() {
-    console.log('Bark');
-  }
+//   bark() {
+//     console.log('Bark');
+//   }
 
-  static diet() {
-    return 'carnivore';
-  }
-}
+//   static diet() {
+//     return 'carnivore';
+//   }
+// }
+let Dog = function (name, age) {
+  this.name = name;
+  this.age = age;
+};
+
+Dog.diet = function () {
+  return 'carnivore';
+};
+
+Dog.prototype.bark = function () {
+  console.log('Bark');
+};
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
