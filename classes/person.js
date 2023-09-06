6ea1215bc59b1bd1806edda71f17a5a762ae0203
Srivatsan <vatsan1993@gmail.com>
2023-09-06 13:19:19 +0530

    finished person es5 class

--- classes/person.js
+++ classes/person.js
@@ -1,25 +1,43 @@
 const Dog = require('./dog');
 
-class Person {
-  constructor(name, age, dogs = []) {
-    this.name = name;
-    this.age = age;
-    this.dogs = dogs;
-  }
+// class Person {
+//   constructor(name, age, dogs = []) {
+//     this.name = name;
+//     this.age = age;
+//     this.dogs = dogs;
+//   }
 
-  addDog(dog) {
-    if (!(dog instanceof Dog)) {
-      throw new TypeError("Can only add dogs!");
-    }
-    this.dogs.push(dog);
-  }
+//   addDog(dog) {
+//     if (!(dog instanceof Dog)) {
+//       throw new TypeError("Can only add dogs!");
+//     }
+//     this.dogs.push(dog);
+//   }
 
-  walkDogs() {
-    this.dogs.forEach((dog) => {
-      console.log(`walking ${dog.name}`);
-    });
+//   walkDogs() {
+//     this.dogs.forEach((dog) => {
+//       console.log(`walking ${dog.name}`);
+//     });
+//   }
+// }
+
+let Person = function (name, age, dogs = []) {
+  this.name = name;
+  this.age = age;
+  this.dogs = dogs;
+};
+Person.prototype.addDog = function (dog) {
+  if (!(dog instanceof Dog)) {
+    throw new TypeError('Can only add dogs!');
   }
-}
+  this.dogs.push(dog);
+};
+
+Person.prototype.walkDogs = function () {
+  this.dogs.forEach((dog) => {
+    console.log(`walking ${dog.name}`);
+  });
+};
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
